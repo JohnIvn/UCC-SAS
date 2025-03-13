04283b7df6aaf417dc0ea87bb7b058ea630de854
Ivan <janivantot@gmail.com>
2025-03-14 00:29:13 +0800

    working signin

--- Frontend/src/pages/landingPage.jsx
+++ Frontend/src/pages/landingPage.jsx
@@ -92,6 +92,7 @@ const LandingPage = () => {
           <>
             <h1>Contact Us</h1>
             <p>If you have any questions, feel free to reach out to us!</p>
+            <br /> <br />
             <Card2 />
           </>
         )}
