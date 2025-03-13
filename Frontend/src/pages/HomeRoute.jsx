bb33f1f91fcc9beb6a9ae9d75980c6dff89d6483
Matthew Cania <matthewgab24@gmail.com>
2025-03-13 10:29:59 +0800

    aside and login page templates

---
+++ Frontend/src/pages/HomeRoute.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./HomePage";
+import MainAside from "../partials/Aside";
+
+
+export default function HomeRoute () {
+
+	return (
+		<section
+			className="flex justify-between items-start h-screen w-screen bg-gray-100"
+		>
+			<MainAside/>
+			
+			<div
+				className="flex flex-col justify-start items-start w-4/5"
+			>
+			<Routes>
+				<Route path="/" element={<HomePage/>}/>
+				{/* <Route path="/" element={}/> */}
+
+			</Routes>
+			</div>
+		</section>
+
+
+	)
+
+}
