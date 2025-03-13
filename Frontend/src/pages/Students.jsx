d1e75547de7b7bd1f32d57b897b43f611937324d
Niggazares <jvazares2004@gmail.com>
2025-03-13 18:17:30 +0800

    Skibidi

--- Frontend/src/pages/Students.jsx
+++ Frontend/src/pages/Students.jsx
@@ -12,27 +12,35 @@ export default function Students() {
 			<div
 				className="flex flex-col justify-between items-start w-full auto h-full auto gap-6"
 			>
-            <div className="flex w-full h-1/4 gap-10 items-center bg-white rounded-xl shadow-2xl mx-auto my-4">
-            <div
-						className='flex justify-between items-center p-10 h-1/2 w-auto lg:w-auto bg-sky-900 relative rounded-xl'
-					>
+				<div className="flex w-full h-auto flex-row justify-between items-center bg-white rounded-xl shadow-2xl mx-auto my-4 p-6 gap-6">
+    
+    <div className="flex justify-between items-center p-6 w-80 h-20 bg-sky-900 relative rounded-xl">
+        <div className="flex w-20 h-20 rounded-full p-2 bg-white absolute left-[-1.5rem] transition-all duration-200">
+            <img className="w-full h-full rounded-full" />
+        </div>
+        <div className="flex flex-col justify-center items-center w-full h-full lg:pl-10">
+            <h1 className="text-white text-md lg:text-xl font-semibold text-center">
+                Bachelor of Science in Information Technology
+            </h1>
+        </div>
+    </div>
 
-						<div
-							className='flex w-20 h-20 lg:w-20 lg:h-20 gap-10 rounded-full p-2 bg-white absolute left-[-1.5rem] transition-all duration-200'
-						>
-							<img
-								className='flex w-full h-full rounded-full'
-								></img>
-						</div>
-						<div
-							className='flex flex-col justify-center items-center w-full h-full pl-28  lg:pl-10'
-						>
-							<h1 className='flex justify-start items-center w-full text-white text-md lg:text-xl font-semibold' >Bachelor Of Science On Information Technology</h1>
+    
+    <div className="flex justify-center items-center p-6 w-64 bg-sky-900 rounded-xl">
+        <h1 className="text-white text-md lg:text-xl font-semibold text-center">
+            Enrolled Students:
+        </h1>
+    </div>
 
-						</div>
-                        
-					</div>
-            </div>
+    
+    <div className="flex justify-center items-center p-6 w-64 bg-sky-900 rounded-xl">
+        <h1 className="text-white text-md lg:text-xl font-semibold text-center">
+            Total Students:
+        </h1>
+    </div>
+</div>
+
+			
 
                 <div className="flex w-1/3 h-auto p-2 gap-2 justify-start items-center bg-white rounded-xl shadow-2xl">
     <form className="flex w-full items-center gap-2">
