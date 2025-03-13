21d4ebeef5af7c7053171a9f12ee99e922a61675
Matthew Cania <matthewgab24@gmail.com>
2025-03-13 18:39:22 +0800

    finished static login page

---
+++ Frontend/src/components/CustomInputA.jsx
@@ -0,0 +1,11 @@
+
+export default function CustomInputA() {
+
+	return (
+		<input
+			className='flex justify-center items-center text-center text-3xl outline-0 w-8 h-10 border-b-2 border-white text-white font-semibold focus:outline-white focus:outline-1 rounded-t-md'
+			type="text"
+			maxLength={1}
+		/>
+	)
+}
--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,9 +1,19 @@
-import TemplateProfile from '../../public/pfp.png'
-import QRCode from '../../public/QR.png'
+import TemplateProfile from '../assets/pfp.png'
+import QRCode from '../assets/QR.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFingerprint } from '@fortawesome/free-solid-svg-icons'
+import SquareButton from '../components/SquareButton'
+import CustomInputA from '../components/CustomInputA'
 
 export default function HomePage() {
+
+	const RenderMultiple = (count, element) => {
+		let elements = []
+		for (let i = 0; i < count; i++) {
+			elements.push(element)
+		}
+		return elements;
+	}
 
 	return (
 		<section
@@ -42,76 +52,77 @@ export default function HomePage() {
 					className="flex justify-center items-start w-full h-2/3 gap-4"
 				>
 					<div
-						className="flex flex-col w-2/3 h-full justify-center items-center gap-4 bg-sky-900 rounded-xl shadow-2xl"
+						className="flex flex-col w-2/3 h-full py-4 justify-start items-center gap-4 bg-sky-900 rounded-xl shadow-2xl"
 					>
-						<h1 className='flex justify-center items-center bg-white text-xl font-semibold w-5/6 rounded-xl h-12'>RFID Scan</h1>	
+						<h1 className='flex justify-center items-center bg-white text-xl font-semibold w-5/6 rounded-xl h-12'>RFID Scan</h1>
 						<img className='flex w-56 h-56' src={QRCode} alt="" />
 					</div>
 
 					<div
 						className="flex flex-col w-1/3 h-full gap-4 py-4 justify-start items-center bg-blue-300 rounded-xl shadow-2xl"
 					>
-						<h1 className='flex justify-center items-center bg-white text-xl font-semibold w-5/6 rounded-xl h-10'>Fingerprint Scan</h1>	
+						<h1 className='flex justify-center items-center bg-white text-xl font-semibold w-5/6 rounded-xl h-10'>Fingerprint Scan</h1>
 						<div
 							className='flex justify-center items-center w-32 h-32 bg-white rounded-2xl'
 						>
-							<FontAwesomeIcon className='flex h-full w-full text-8xl' icon={faFingerprint}/>
+							<FontAwesomeIcon className='flex h-full w-full text-8xl' icon={faFingerprint} />
 						</div>
 					</div>
 				</div>
 			</div>
 
 
-			<div
+			<form
 				className="flex flex-col justify-center items-center w-2/5 h-full bg-blue-300 rounded-xl shadow-2xl gap-4"
 			>
 				<h1 className='flex justify-center items-center bg-white text-xl font-semibold w-5/6 rounded-xl h-10'>
 					Student OTP
-				</h1>	
+				</h1>
 
 				<div
 					className='flex justify-center items-center h-40 w-5/6 bg-gray-800 rounded-2xl gap-2'
 				>
-					<input 
-						className='flex justify-center items-center text-center text-3xl outline-0 w-8 h-10 border-b-2 border-white text-white font-semibold focus:outline-white focus:outline-1 rounded-t-md' 
-						type="text"
-						maxLength={1}
-						/>
-					<input 
-						className='flex justify-center items-center text-center text-3xl outline-0 w-8 h-10 border-b-2 border-white text-white font-semibold focus:outline-white focus:outline-1 rounded-t-md' 
-						type="text"
-						maxLength={1}
-						/>
-					<input 
-						className='flex justify-center items-center text-center text-3xl outline-0 w-8 h-10 border-b-2 border-white text-white font-semibold focus:outline-white focus:outline-1 rounded-t-md' 
-						type="text"
-						maxLength={1}
-						/>
-					<input 
-						className='flex justify-center items-center text-center text-3xl outline-0 w-8 h-10 border-b-2 border-white text-white font-semibold focus:outline-white focus:outline-1 rounded-t-md' 
-						type="text"
-						maxLength={1}
-						/>
-					<input 
-						className='flex justify-center items-center text-center text-3xl outline-0 w-8 h-10 border-b-2 border-white text-white font-semibold focus:outline-white focus:outline-1 rounded-t-md' 
-						type="text"
-						maxLength={1}
-						/>
-					<input 
-						className='flex justify-center items-center text-center text-3xl outline-0 w-8 h-10 border-b-2 border-white text-white font-semibold focus:outline-white focus:outline-1 rounded-t-md' 
-						type="text"
-						maxLength={1}
-						/>
+					{RenderMultiple(6, <CustomInputA />)}
+
 				</div>
 
 				<div
-					className='flex'
+					className='flex flex-col w-2/3 h-1/2 justify-between items-center'
 				>
+					<div
+						className='flex w-full h-auto justify-between items-center'
+					>
+						<SquareButton text={1} size={20} type={"button"} />
+						<SquareButton text={2} size={20} type={"button"} />
+						<SquareButton text={3} size={20} type={"button"} />
+					</div>
 
+					<div
+						className='flex w-full h-auto justify-between items-center'
+					>
 
+						<SquareButton text={4} size={20} type="button" />
+						<SquareButton text={5} size={20} type="button" />
+						<SquareButton text={6} size={20} type="button" />
+					</div>
+
+					<div
+						className='flex w-full h-auto justify-between items-center'
+					>
+						<SquareButton text={7} size={20} type={"button"} />
+						<SquareButton text={8} size={20} type={"button"} />
+						<SquareButton text={9} size={20} type={"button"} />
+					</div>
+					<div
+						className='flex w-full h-auto justify-between items-center'
+					>
+						<SquareButton text={"Delete"} size={20} type={"button"} />
+						<SquareButton text={0} size={20} type={"button"} />
+						<SquareButton text={"Enter"} size={20} type={"button"} />
+					</div>
 				</div>
 
-			</div>
+			</form>
 
 
 
